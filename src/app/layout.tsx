5e3a8ef53e99ce759ede9414ce11236f450dafca
atsuki <0601m0429@gmail.com>
2025-04-25 10:25:32 +0900

    ブラウザのタイトルバー表示

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,12 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ReactNode } from "react";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'atsuki | portfolio',
+  description: 'Atsukiのポートフォリオサイト',
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
